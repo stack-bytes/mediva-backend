4f5b20918c11955aa893c3b6a1d6e901a64da8a5
Jarvx200 <jarvx404@gmail.com>
2024-11-08 21:53:45 +0200

    started working on appointment and tweaks

---
+++ appointment-service/src/main/java/com/stackbytes/AppointmentMain.java
@@ -0,0 +1,11 @@
+package com.stackbytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentMain {
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentMain.class, args);
+    }
+}
